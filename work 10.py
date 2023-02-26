103f44456bc100e75eff3c9d61d111462909471f
zonenvsev <123860860+zonenvsev@users.noreply.github.com>
2023-02-26 23:09:24 +0200

    Add files via upload

---
+++ work 10.py
@@ -0,0 +1,9 @@
+class Iter:
+	def __iter__(selh, data):
+		self.data = data
+	def __iter__(self):
+		for eter in self data:
+			yield item
+iteabl = Iter([1,2,3,4,5])
+for eter in iteabl:
+	print(item)
